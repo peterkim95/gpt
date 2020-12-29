27e0ecbacc38e0f21494603598f554a3f9dd0280
Peter Kim <peter.kim@buzzvil.com>
2020-12-29 15:00:11 +0900

    test with notebook

---
+++ model.py
@@ -0,0 +1,55 @@
+import math
+import torch
+import torch.nn as nn
+from torch.nn import TransformerDecoder, TransformerDecoderLayer
+import torch.nn.functional as F
+
+
+class Transformer_Decoder(nn.Module):
+
+    def __init__(self, ntoken, ninp, nhead, nhid, nlayers, dropout=0.1):
+        super(Transformer_Decoder, self).__init__()
+        self.model_type = 'Transformer-Decoder (T-D)'
+        self.pos_encoder = PositionalEncoding(ninp, dropout)
+        decoder_layers = TransformerDecoderLayer(ninp, nhead, nhid, dropout)
+        self.transformer_decoder = TransformerDecoder(decoder_layers, nlayers)
+        self.embedding_encoder = nn.Embedding(ntoken, ninp)
+        self.ninp = ninp
+        self.final_layer = nn.Linear(ninp, ntoken)
+
+        self.init_weights()
+
+    def generate_square_subsequent_mask(self, sz):
+        mask = (torch.triu(torch.ones(sz, sz)) == 1).transpose(0, 1)
+        mask = mask.float().masked_fill(mask == 0, float('-inf')).masked_fill(mask == 1, float(0.0))
+        return mask
+
+    def init_weights(self):
+        initrange = 0.1
+        self.embedding_encoder.weight.data.uniform_(-initrange, initrange)
+
+    def forward(self, src, src_mask):
+        src = self.embedding_encoder(src) * math.sqrt(self.ninp)
+        src = self.pos_encoder(src)
+        output = self.transformer_decoder(tgt=src, memory=src, tgt_mask=src_mask)
+        output = self.final_layer(output)
+        return output
+
+
+class PositionalEncoding(nn.Module):
+
+    def __init__(self, d_model, dropout=0.1, max_len=5000):
+        super(PositionalEncoding, self).__init__()
+        self.dropout = nn.Dropout(p=dropout)
+
+        pe = torch.zeros(max_len, d_model)
+        position = torch.arange(0, max_len, dtype=torch.float).unsqueeze(1)
+        div_term = torch.exp(torch.arange(0, d_model, 2).float() * (-math.log(10000.0) / d_model))
+        pe[:, 0::2] = torch.sin(position * div_term)
+        pe[:, 1::2] = torch.cos(position * div_term)
+        pe = pe.unsqueeze(0).transpose(0, 1)
+        self.register_buffer('pe', pe)
+
+    def forward(self, x):
+        x = x + self.pe[:x.size(0), :]
+        return self.dropout(x)
