61f09e7aba9be32a4e769c355c4743879d893b14
Peter Kim <peter.kim@buzzvil.com>
2021-01-04 10:02:36 +0900

    change default ninp

--- utils.py
+++ utils.py
@@ -6,7 +6,7 @@ import torch
 def get_args():
     parser = ArgumentParser()
 
-    parser.add_argument('--ninp', type=int, default=100)
+    parser.add_argument('--ninp', type=int, default=200)
     parser.add_argument('--nhead', type=int, default=2)
     parser.add_argument('--nhid', type=int, default=200)
     parser.add_argument('--nlayers', type=int, default=2)
